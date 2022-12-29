3a0522f683ff3cf07c6ceeadccf5b5de6353dd78
Stephen Smith <steve@skysmith.io>
2022-12-29 07:45:37 -0800

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Stephen Smith on 12/29/22.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
---
+++ RickAndMorty/APIClient/RMService.swift
@@ -0,0 +1,28 @@
+//
+//  RMService.swift
+//  RickAndMorty
+//
+//  Created by Stephen Smith on 12/29/22.
+//
+// Singleton: object accessible anywhere in the app
+
+import Foundation
+
+
+/// Primary API service object to get Rick and Morty data
+final class RMService {
+    
+    /// Shared singleton instance
+    static let shared = RMService()
+    
+    /// Privatized constructor
+    private init() {}
+    
+    /// Send Rick and Morty API Call
+    /// - Parameters:
+    ///   - request: Request instance
+    ///   - completion: Callback with data or error
+    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+        
+    }
+}
